9514dc981e9a734390e96366ab45854f246a4a2c
ayaz <ayazhussainbs@gmail.com>
2020-10-20 09:38:54 +0500

    User fixed

--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux'
 import customerReducer from './Customer/customerReducer'
 import jobReducer from './Job/jobReducer'
 import claimReducer from './Claims/claimsReducers'
-import userReducer from './User/userReducer'
+import userReducer from './user/userReducer'
 
 
 var rootReducer = combineReducers({
-customers: customerReducer,
-jobs: jobReducer,
-claims: claimReducer,
-users: userReducer
+    customers: customerReducer,
+    jobs: jobReducer,
+    claims: claimReducer,
+    users: userReducer
 })
 export default rootReducer

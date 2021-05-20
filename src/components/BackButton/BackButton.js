7c1d366a7ee5d8fdb2c9db3f9d76a262c77f26a5
Taha-Imtiaz <tahaimtiaz1996@gmail.com>
2021-05-20 18:09:15 +0500

    final athens code

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,19 +1,21 @@
-import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 import style from "./BackButton.module.css";
 import { useHistory } from "react-router-dom";
 
-
 const BackButton = () => {
-    let history = useHistory();
-    return (
-        <div className={style.backButtonMain}>
-            <div className={style.backButton}>
-                <FontAwesomeIcon icon={faArrowCircleLeft} onClick={()=> history.goBack()} />
-            </div>
-        </div>
-    )
-}
+  let history = useHistory();
+  return (
+    <div className={style.backButtonMain}>
+      <div className={style.backButton}>
+        <FontAwesomeIcon
+          icon={faArrowCircleLeft}
+          onClick={() => history.goBack()}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default BackButton
+export default BackButton;
